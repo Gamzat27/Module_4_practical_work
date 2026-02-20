d02ea5650a0c94b4abf2cc7e26b3165feae729e9
Gamzat <gamzat27@icloud.com>
2026-02-21 02:25:38 +0300

    added constants.py

---
+++ constants.py
@@ -0,0 +1,16 @@
+
+BASE_URL_MOVIES = "https://api.dev-cinescope.coconutqa.ru" # базовый url для фильмов
+MOVIES_ENDPOINT = "/movies" # фильмы
+GENRES_ENDPOINT = "/genres" # жанры
+
+# Заголовки для запросов
+HEADERS = {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+}
+
+# Креды админа (имеет все роли)
+ADMIN_CRED = {
+    "username": "api1@gmail.com",
+    "password": "asdqwe123Q"
+}

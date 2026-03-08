3b6c22ae58647ca2ab78c4ed4b98d5f2beabd721
Gamzat <gamzat27@icloud.com>
2026-03-08 21:19:31 +0300

    added pydantic model in tests

--- tests/api/test_user.py
+++ tests/api/test_user.py
@@ -1,28 +1,26 @@
 
 import pytest
+from models.pydantic_models import RegisterUserResponse
 
 class TestUser:
 
     def test_create_user(self, super_admin, creation_user_data):
         response = super_admin.api.user_api.create_user(creation_user_data, expected_status=201).json()
-
-        assert response.get('id') and response['id'] != '', "ID должен быть не пустым"
-        assert response.get('email') == creation_user_data.email
-        assert response.get('fullName') == creation_user_data.fullName
-        assert response.get('roles', []) == creation_user_data.roles
-        assert response.get('verified') is True
+        created_user_response = RegisterUserResponse(**response)
+        assert created_user_response.email == creation_user_data.email, "Email не совпадает"
+        assert created_user_response.email == creation_user_data.email, "Email не совпадает"
+        assert created_user_response.id, "ID должен быть не пустым"
+        assert created_user_response.fullName == creation_user_data.fullName
+        assert created_user_response.roles == creation_user_data.roles
+        assert created_user_response.verified is True
 
     def test_get_user_by_locator(self, super_admin, creation_user_data):
         created_user_response = super_admin.api.user_api.create_user(creation_user_data, expected_status=201).json()
-        response_by_id = super_admin.api.user_api.get_user_info(created_user_response['id']).json()
-        response_by_email = super_admin.api.user_api.get_user_info(creation_user_data.email).json()
-
-        assert response_by_id == response_by_email, "Содержание ответов должно быть идентичным"
-        assert response_by_id.get('id') and response_by_id['id'] != '', "ID должен быть не пустым"
-        assert response_by_id.get('email') == creation_user_data.email
-        assert response_by_id.get('fullName') == creation_user_data.fullName
-        assert response_by_id.get('roles', []) == creation_user_data.roles
-        assert response_by_id.get('verified') is True
+        created_user_response = RegisterUserResponse(**created_user_response)
+        assert created_user_response.id is not None, "Поле id не должно быть пустым."
+        assert created_user_response.email == creation_user_data.email, "email должны быть равны."
+        assert created_user_response.roles == creation_user_data.roles, "Роли должны совпадать."
+        assert created_user_response.fullName == creation_user_data.fullName, "Имя должно совпадать."
 
     @pytest.mark.slow
     def test_get_user_by_id_common_user(self, common_user):
